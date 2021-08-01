ae6b950c6ce5d45a428f3bcc77d0f05c4cab228c
Jeffrey Symens <jeffrey.symens23@gmail.com>
2021-08-01 16:01:04 -0500

    chage about page to workout page

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
 import Signup from '../views/Signup.vue'
-import About from '../views/About.vue'
+import Workout from '../views/Workout.vue'
 import Login from "../views/Login.vue";
 import Logout from "../views/Logout.vue";
 
@@ -11,7 +11,7 @@ Vue.use(VueRouter)
 
 const routes = [
   { path: '/', name: 'Home', component: Home },
-  { path: '/about', name: 'About', component: About },
+  { path: '/workout', name: 'Workout', component: Workout },
   { path: "/signup", name: "signup", component: Signup },
   { path: "/login", name: "login", component: Login },
   { path: "/logout", name: "logout", component: Logout },
