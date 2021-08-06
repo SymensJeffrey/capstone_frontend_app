d9224dcff8001e0fcb676f1d14f5e35c08756a32
Jeffrey Symens <jeffrey.symens23@gmail.com>
2021-08-05 19:01:49 -0500

    add workout index

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -1,19 +1,36 @@
 <template>
   <div class="home">
-    <h1>{{ message }}</h1>
+    <h1>Hello </h1>
+    <div v-for=" workout in workouts">
+      <p>ID: {{ workout.id }}</p>
+      <p>User ID: {{ workout.user_id }}</p>
+      <p>Date: {{ workout.date }}</p>
+
+    </div>
+    </div>
   </div>
 </template>
 
 <style></style>
 
 <script>
+  import axios from "axios"
   export default {
     data: function () {
       return {
-        message: "Welcome to Vue.js!",
+        workouts: []
       };
     },
-    created: function () {},
-    methods: {},
+    created: function () {
+      this.workoutIndex()
+    },
+    methods: {
+      workoutIndex: function() {
+        console.log("workout index...")
+        axios.get("/workouts").then((response) => {console.log("workout index", response); 
+        this.workouts = response.data;
+      });
+      },
+    },
   };
 </script>
