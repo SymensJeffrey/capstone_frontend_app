16fe0bb8cf43f93182fddaa9f3390b7c1a3d6a98
Jeffrey Symens <jeffrey.symens23@gmail.com>
2021-08-05 19:39:48 -0500

    add workout show dialog box

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -5,8 +5,22 @@
       <p>ID: {{ workout.id }}</p>
       <p>User ID: {{ workout.user_id }}</p>
       <p>Date: {{ workout.date }}</p>
-
+      <p><button v-on:click="workoutShow(workout)">View Workout</button></p>
     </div>
+    <dialog id="workout-details">
+      <form method="dialog">
+        <p> {{currentWorkout.id}} </p>
+        <p> {{currentWorkout.date}} </p>
+        <p> {{currentWorkout.lift}}
+        <div v-for="lift in lifts">
+          <p>Name:{{lift.exercise_id}} |
+          Weight:{{lift.weight}} lbs |
+          Reps:{{lift.reps}} |
+          Sets{{lift.sets}} </p>
+        </div>
+        <button>Close</button>
+      </form>
+    </dialog>
     </div>
   </div>
 </template>
@@ -19,7 +33,9 @@
     data: function () {
       return {
         workouts: [],
-        user: ""
+        user: "",
+        currentWorkout: {},
+        lifts: []
       };
     },
     created: function () {
@@ -38,6 +54,14 @@
         this.user = response.data;
         });
       },
+      workoutShow: function(workout) {
+        this.currentWorkout = workout
+        axios.get("/workouts/8").then((response) => {console.log("workoutshow", response);
+        this.currentWorkout = response.data;
+        this.lifts = this.currentWorkout.lifts
+        })
+        document.querySelector("#workout-details").showModal();
+      }
     },
   };
 </script>
