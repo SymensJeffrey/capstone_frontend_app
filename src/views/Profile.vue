da1504480eb5e9d7bbf2c7ee00adc470583299d4
Jeffrey Symens <jeffrey.symens23@gmail.com>
2021-08-05 19:22:22 -0500

    add user show to render first name on the page

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="home">
-    <h1>Hello </h1>
+    <h1>Hello {{ user.name }}</h1>
     <div v-for=" workout in workouts">
       <p>ID: {{ workout.id }}</p>
       <p>User ID: {{ workout.user_id }}</p>
@@ -18,11 +18,13 @@
   export default {
     data: function () {
       return {
-        workouts: []
+        workouts: [],
+        user: ""
       };
     },
     created: function () {
-      this.workoutIndex()
+      this.workoutIndex(),
+      this.userShow()
     },
     methods: {
       workoutIndex: function() {
@@ -30,6 +32,11 @@
         axios.get("/workouts").then((response) => {console.log("workout index", response); 
         this.workouts = response.data;
       });
+      },
+      userShow: function() {
+        axios.get(`/users/7`).then((response) => {console.log(response)
+        this.user = response.data;
+        });
       },
     },
   };
