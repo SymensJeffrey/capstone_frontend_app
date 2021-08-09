0f4bee404a79bfd9edcce84792057785c45ba684
Jeffrey Symens <jeffrey.symens23@gmail.com>
2021-08-09 18:30:57 -0500

    commit before trying to install a theme

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@
       <router-link to="/">Home</router-link> |
       <router-link to="/workout">Workout</router-link> | 
       <router-link v-if="!isLoggedIn()" to="/signup">Signup</router-link> | 
-      <router-link v-if="!isLoggedIn()" to="/login">Login</router-link> |
+      <router-link v-if="!isLoggedIn()" to="/login">Login</router-link> 
       <router-link v-if="isLoggedIn()" to="/logout">Logout</router-link> |
       <router-link v-if="isLoggedIn()" to="/profile">Profile</router-link>
       <!-- change to {{user.name}} when making dynamic -->
