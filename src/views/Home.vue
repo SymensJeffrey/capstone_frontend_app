58331fb090e4e7531b4c85b06bfa11867862620d
Jeffrey Symens <jeffrey.symens23@gmail.com>
2021-08-09 19:44:05 -0500

    add hero/photo carosule and add images

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,38 @@
 <template>
   <div class="home">
-    <h1>{{ message }}</h1>
+    <br>
+    <br>
+    <section id="hero">
+      <div id="heroCarousel" data-bs-interval="5000" class="carousel slide carousel-fade" data-bs-ride="carousel">
+
+        <!-- This gives me an error <ol class="carousel-indicators" id="hero-carousel-indicators"></ol> -->
+
+        <div class="carousel-inner" role="listbox">
+
+          <!-- Slide 1 -->
+          <div class="carousel-item active" style="background-image: url(assets/img/slide/slide-1.jpg)">
+          </div>
+
+          <!-- Slide 2 -->
+          <div class="carousel-item" style="background-image: url(assets/img/slide/slide-2.jpeg)">
+          </div>
+
+          <!-- Slide 3 -->
+          <div class="carousel-item" style="background-image: url(assets/img/slide/slide-3.jpg)">
+          </div>
+
+        </div>
+
+        <a class="carousel-control-prev" href="#heroCarousel" role="button" data-bs-slide="prev">
+          <span class="carousel-control-prev-icon bi bi-chevron-left" aria-hidden="true"></span>
+        </a>
+
+        <a class="carousel-control-next" href="#heroCarousel" role="button" data-bs-slide="next">
+          <span class="carousel-control-next-icon bi bi-chevron-right" aria-hidden="true"></span>
+        </a>
+
+      </div>
+    </section><!-- End Hero -->
     <hr />
     <div v-for="exercise in exercises">
       <p>ID:{{ exercise.id }}</p>
