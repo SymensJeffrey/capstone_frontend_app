d65fe2b02018646521cd09b3aae4876560e29f97
Jeffrey Symens <jeffrey.symens23@gmail.com>
2021-08-10 19:47:23 -0500

    remove footer

--- src/App.vue
+++ src/App.vue
@@ -50,7 +50,7 @@
     </header>
     <router-view/>
     <div>
-      <footer id="footer">
+      <!-- <footer id="footer">
         <div class="container">
           <h3>GYMOLOGY</h3>
           <p>Et aut eum quis fuga eos sunt ipsa nihil. Labore corporis magni eligendi fuga maxime saepe commodi placeat.</p>
@@ -68,7 +68,7 @@
             Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
           </div>
         </div>
-      </footer>
+      </footer> -->
     </div>
   </div>
 </template>
